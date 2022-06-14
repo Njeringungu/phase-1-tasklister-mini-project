8aa970964c9364c4f7c126ae6a79384b83546829
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-06-14 08:03:09 +0300

    adding mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const taskItem = document.getElementById("create-task-form");
+  const newTaskItem = document.getElementById("new-task-description");
+  const newPriorityTask = document.getElementById("new-task-priority");
+  const newTaskUl = document.getElementById("tasks");
+  taskItem.addEventListener("submit", createNewTask);
 });
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskItem = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskItem.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+
+};
